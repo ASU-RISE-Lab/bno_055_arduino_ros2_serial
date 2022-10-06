f592c56ce11efab015c611030cf19848fa19bb95
Aravind-Adhith <aravindadhith@asu.edu>
2022-10-06 11:02:06 -0700

    Minor Changes

--- src/serial_link.py
+++ src/serial_link.py
@@ -1,17 +1,3 @@
-# Copyright 2016 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import rclpy
 import serial
 from rclpy.node import Node
@@ -23,7 +9,7 @@ class BNO055_DATA(Node):
 
     def __init__(self):
         super().__init__('BNO055_DATA')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(String, 'topic', 1)
         timer_period = 0.02  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.device = serial.Serial("/dev/ttyACM0", 115200, timeout=0.5)
